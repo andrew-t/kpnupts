68215dff35653b59f7dc1b58d832a16075ae036d
Andrew Taylor <andrew.taylor04@bbc.co.uk>
2016-05-23 13:00:17 +0100

    init commit

---
+++ js-src/main.js
@@ -0,0 +1,11 @@
+// Game board dimensions:
+var cols = 7,
+	pusherRows = 4,
+	pusherMotion = 2,
+	plateRows = 10;
+
+// Initialise the empty grid
+var grid = [];
+for (let i = 0; i < cols; ++i)
+	grid.push([]);
+
